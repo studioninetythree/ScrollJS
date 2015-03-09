c4a7302322fcb6877e0a7d8b23a25b26b5ec5f03
Dillan <211299820@cput.ac.za>
2015-03-09 04:12:40 -0700

    Initial commit

---
+++ scroll.js
@@ -0,0 +1,48 @@
+/* ========================================================================
+ * Ninetythree: scroll.js v1.0.0
+ * Dillan Isaacs
+ * March 8, 2015
+ * http://getbootstrap.com/javascript/#transitions
+ * ========================================================================
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+ * ========================================================================
+ * This script works most effectively with Single-Page Sites (SPS's).
+ * It is used to animate auto-scrolling withing the web page using.
+ * Just call the function and pass in the ID of the element you would like the page to scroll to.
+ * The variable 'scrollSpeed' can be increased and decreased to whichever speed you'd like.
+ * */
+
+var scrollSpeed = 24;
+var distance = 80;
+var scrollY = 0;
+
+function scrollTo(el)
+{
+    var currentY = window.pageYOffset;
+    var targetY = document.getElementById(el).offsetTop;
+    var documentHeight = document.body.offsetHeight;
+    var yPosition = currentY + window.innerHeight;
+    var scroller = setTimeout('scrollTo(\''+el+'\')', scrollSpeed);
+
+    if(yPosition > documentHeight)
+    {
+        clearTimeout(scroller);                     // Stops the scroller from scrolling forever
+    }
+    else
+    {
+        if(currentY < (targetY - distance))         // To scroll to contents below the current element
+        {
+            scrollY = currentY + distance;
+            window.scroll(0, scrollY);
+        }
+        else if(currentY > targetY)                 // To scroll to contents above the current element
+        {
+            scrollY = currentY - distance;
+            window.scroll(0, scrollY);
+        }
+        else
+        {
+            clearTimeout(scroller);
+        }
+    }
+}
